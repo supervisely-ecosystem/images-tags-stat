568f588a36fb215e28921b82769407991c47330f
Alex Emelyanov <alex84_84_84@mail.ru>
2021-07-12 17:42:43 +0300

    test 11, 12 tables disabled

--- src/classes_stat.py
+++ src/classes_stat.py
@@ -535,9 +535,12 @@ def my_test_select(api: sly.Api, task_id, context, state, app_logger):
     df_12 = get_pd_tag_stat_12(meta, datasets_counts_12, columns_objects_tags_12, obj_tags_to_vals)  # 12
     print(df_12)                                                                                     # 12
 
-    if len(objects_tags) == 0:
+    if len(objects_tags) != 0 and len(state['choose_objs_tags']) == 0:
         table_11 = {"field": "state.ObjTagsNoExist", "payload": True}
-        table_12 = {"field": "state.ObjTagsNoExist", "payload": True}
+        table_12 = {"field": "state.noObjects", "payload": False}
+    elif len(objects_tags) == 0:
+        table_11 = {"field": "state.noObjects", "payload": False}
+        table_12 = {"field": "state.noObjects", "payload": False}
     else:
         table_11 = {"field": "data.obj_tags_to_classes_statTable", "payload": json.loads(df_11.to_json(orient="split"))}
         table_12 = {"field": "data.obj_tags_vals_to_classes_statTable", "payload": json.loads(df_12.to_json(orient="split"))}
